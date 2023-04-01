3fd988d1e2e2d8a2ea52c8ee0387d9cf5cdc1fbf
Shadman Sakib <shadman@felicity.digital>
2023-04-02 00:08:57 +0600

    Refactor statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  var app = MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Lets see some images"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add_a_photo),
-      ),
-    ),
-  );
+import 'src/app.dart';
 
-  runApp(app);
+void main() {
+  runApp(App());
 }
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,13 +1,32 @@
-class name extends StatefulWidget {
-  const name({super.key});
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  const App({super.key});
 
   @override
-  State<name> createState() => _nameState();
+  State<App> createState() => _AppState();
 }
 
-class _nameState extends State<name> {
+class _AppState extends State<App> {
+  int counter = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Let's see some images"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() => counter += 1);
+          },
+          child: Icon(Icons.add_a_photo),
+        ),
+        body: Center(
+          child: Text('$counter'),
+        ),
+      ),
+    );
   }
 }
